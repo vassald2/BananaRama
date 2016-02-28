c06bd6dd1698c5b21cb9fa7ae554e25763897e67
David Vassallo <dvassallo4427@gmail.com>
2016-02-28 03:29:39 -0500

    finished?

--- BananaTime.js
+++ BananaTime.js
@@ -4,14 +4,11 @@ window.setInterval(function(){
 
     function changeImage() {
         for (var count = 0; count < imgs.length; count++) {
-            var selector = Math.floor((Math.random() * 2) + 1);
-            if(imgs[count].complete!=="true") {
+                var selector = Math.floor((Math.random() * 2) + 1);
                 if (selector === 1)
                     imgs[count].src = "http://www.sherv.net/cm/emo/funny/2/big-dancing-banana-smiley-emoticon.gif";
                 else if (selector === 2)
                     imgs[count].src = "http://rs1215.pbsrc.com/albums/cc506/Rockingbro/170422_dancing_banana.gif~c200";
-                imgs[count].setAttribute("complete", "true");
-            }
         }
     }
 },1000);
