1fdf747f75ba1c5d482496d3a33c1b4eeecc16a0
David Vassallo <dvassallo4427@gmail.com>
2016-02-27 22:34:05 -0500

    diff bananas

--- BananaTime.js
+++ BananaTime.js
@@ -1,11 +1,17 @@
-
+window.setInterval(function(){
     var imgs = document.getElementsByTagName("img");
     changeImage();
 
-    function changeImage(e) {
+    function changeImage() {
         for (var count = 0; count < imgs.length; count++) {
-            imgs[count].src = "http://rs1215.pbsrc.com/albums/cc506/Rockingbro/170422_dancing_banana.gif~c200";
+            var selector = Math.floor((Math.random() * 2) + 1);
+            if(imgs[count].complete!=="true") {
+                if (selector === 1)
+                    imgs[count].src = "http://www.sherv.net/cm/emo/funny/2/big-dancing-banana-smiley-emoticon.gif";
+                else if (selector === 2)
+                    imgs[count].src = "http://rs1215.pbsrc.com/albums/cc506/Rockingbro/170422_dancing_banana.gif~c200";
+                imgs[count].setAttribute("complete", "true");
+            }
         }
     }
-    window.addEventListener("DOMContentLoaded",changeImage);
-
+},1000);
